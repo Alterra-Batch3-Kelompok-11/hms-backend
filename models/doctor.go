e66d216cc401a8c1db4d4db4ffd0f48d1ad3f0df
unknown <abdiltegar12@gmail.com>
2022-11-23 15:43:20 +0700

    fixing model doctor

--- models/doctor.go
+++ models/doctor.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Doctor struct {
 	gorm.Model
 	UserId        uint   `json:"user_id" form:"user_id"`
-	SpecialityId  uint   `json:"user_id" form:"user_id"`
+	SpecialityId  uint   `json:"speciality_id" form:"speciality_id"`
 	LicenseNumber string `json:"license_number" form:"license_number"`
 	User          User
 	Speciality    Speciality
