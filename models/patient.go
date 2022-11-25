eb20e64f1e3c90e593545d4662d437656e727837
unknown <abdiltegar12@gmail.com>
2022-11-25 15:49:56 +0700

    for pull

--- models/patient.go
+++ models/patient.go
@@ -13,7 +13,7 @@ type Patient struct {
 	Nik           string    `json:"nik" form:"nik"`
 	Name          string    `json:"name" form:"name"`
 	BirthDate     time.Time `json:"birth_date" form:"birth_date"`
-	Gender        bool      `json:"gender" form:"gender"`
+	Gender        int       `json:"gender" form:"gender"`
 	Address       string    `json:"address" form:"address"`
 	MaritalStatus bool      `json:"marital_status" form:"address"`
 	Religion      Religion
