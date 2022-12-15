162754a6a6e5cfd880ac7d4467bf83f48806e452
unknown <abdiltegar12@gmail.com>
2022-12-15 12:27:20 +0700

    fixing crud patient & swagger

--- dto/patient.go
+++ dto/patient.go
@@ -1,6 +1,7 @@
 package dto
 
 import (
+	"gorm.io/gorm"
 	"time"
 
 	"github.com/go-playground/validator/v10"
@@ -15,6 +16,21 @@ type Patient struct {
 	Address       string    `json:"address" validate:"required"`
 	MaritalStatus bool      `json:"marital_status"`
 	ReligionID    uint      `json:"religion_id"`
+}
+
+type PatientRes struct {
+	ID            uint           `json:"id" form:"id"`
+	CreatedAt     time.Time      `json:"created_at" form:"created_at"`
+	UpdatedAt     time.Time      `json:"updated_at" form:"updated_at"`
+	DeletedAt     gorm.DeletedAt `gorm:"index" json:"deleted_at" form:"deleted_at"`
+	Nik           string         `json:"nik" form:"nik"`
+	Name          string         `json:"name" form:"name"`
+	BirthDate     time.Time      `json:"birth_date" form:"birth_date"`
+	Gender        int            `json:"gender" form:"gender"`
+	Address       string         `json:"address" form:"address"`
+	Phone         string         `json:"phone" form:"phone"`
+	MaritalStatus bool           `json:"marital_status" form:"marital_status"`
+	ReligionID    uint           `json:"religion_id" form:"religion_id"`
 }
 
 func (ctrl *Patient) Validate() error {
